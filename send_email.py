40396b49f48ed1216b517f717f7c56d370b10b93
uygar <uygargurnal2@gmail.com>
2024-09-12 15:51:38 +0300

    gizliii

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "infocukamuran@gmail.com"
-    password = "zqusoeboryzgiskp"
+    password = os.getenv("PASS3")
     reciever = "infocukamuran@gmail.com"
     context = ssl.create_default_context()
 
